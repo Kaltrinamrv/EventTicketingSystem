eb2e349dc030256280da03810c7ed295a1c6af4d
Kaltrinamrv <153457397+Kaltrinamrv@users.noreply.github.com>
2024-04-29 13:35:02 +0200

    clean code

--- backend/Entities/Ticket.cs
+++ backend/Entities/Ticket.cs
@@ -4,10 +4,8 @@ namespace backend.Entities
 {
     public class Ticket
     {
-        public int TicketID { get; set; }
-        
+        public int TicketID { get; set; }     
         public int EventID { get; set; }
-       
         public Event Event { get; set; }
         public bool IsAvailable { get; set; }
         public string TicketType { get; set; }
